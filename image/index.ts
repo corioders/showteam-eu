daf4af3d1fdaaa57589fac7067c0f255c1c668c5
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-01-19 16:38:23 +0100

    add license

--- image/index.ts
+++ image/index.ts
@@ -1,3 +1,8 @@
+// Copyright (C) Corioders <corioders@gmail.com> - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// Written by Wiktor Jurkiewicz <watjurk@gmail.com> and Artur Mucowski <artur@mucowski.pl>, January 2025
+
 import type { StaticImageData, StaticImport, StaticRequire } from 'next/dist/shared/lib/get-img-props';
 
 export type ImageSource = string | StaticImport;
