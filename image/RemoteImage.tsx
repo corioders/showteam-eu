d685e8b0a6e9d2ea910d1e85107f09fa1ff99f49
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-01-21 11:05:05 +0100

    cleanup the different image types

---
+++ image/RemoteImage.tsx
@@ -0,0 +1,68 @@
+// Copyright (C) Corioders <corioders@gmail.com> - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// Written by Wiktor Jurkiewicz <watjurk@gmail.com> and Artur Mucowski <artur@mucowski.pl>, January 2025
+
+import 'server-only';
+
+import NextExportOptimizeRemotePicture from 'next-export-optimize-images/remote-picture.js';
+import type { ImageProps as ImagePropsNext } from 'next/dist/shared/lib/get-img-props';
+
+import { imageSize } from 'cstd-ts/media/image/size.js';
+
+import './picture-display-style.css';
+import type { RemoteImageSource } from './index.ts';
+
+export type RemoteImageProps = {
+	src: RemoteImageSource;
+} & Omit<ImagePropsNext, 'src'>;
+
+export default async function RemoteImage(props: RemoteImageProps) {
+	const nextImageProps = { ...props };
+
+	if (!isValidURL(nextImageProps.src)) {
+		throw new Error(`RemoteImage: The 'src' prop should be a remote image URL.`);
+	}
+
+	const [size, err] = await imageSize(nextImageProps.src);
+	if (err !== null) {
+		// TODO: fix error handling
+		throw err;
+	}
+
+	if (nextImageProps.sizes === undefined) {
+		nextImageProps.sizes = '100vh';
+	}
+
+	nextImageProps.width = size.width;
+	nextImageProps.height = size.height;
+
+	return <NextExportOptimizeRemotePicture {...nextImageProps} />;
+}
+
+function isValidURL(x: string): boolean {
+	if (typeof x !== 'string') {
+		return false;
+	}
+
+	try {
+		new URL(x);
+		return true;
+	} catch {
+		return false;
+	}
+}
+
+export interface PreloadRemoteImagesProps {
+	sources: RemoteImageSource[];
+}
+
+export function PreloadRemoteImages(props: PreloadRemoteImagesProps) {
+	return (
+		<div style={{ display: 'none' }}>
+			{props.sources.map((source) => {
+				return <RemoteImage style={{ display: 'none' }} key={`PreloadRemoteImages${source}`} src={source} alt={'PreloadRemoteImages'} loading="eager" priority={false} />;
+			})}
+		</div>
+	);
+}
--- image/index.ts
+++ image/index.ts
@@ -3,30 +3,7 @@
 // Proprietary and confidential
 // Written by Wiktor Jurkiewicz <watjurk@gmail.com> and Artur Mucowski <artur@mucowski.pl>, January 2025
 
-import type { StaticImageData, StaticImport, StaticRequire } from 'next/dist/shared/lib/get-img-props';
+import type { StaticImport } from 'next/dist/shared/lib/get-img-props';
 
-export type ImageSource = string | StaticImport;
-
-export function normalizeImageSource(imageSource: ImageSource): StaticImageData | string {
-	if (typeof imageSource === 'string') {
-		return imageSource;
-	}
-
-	if (isStaticRequire(imageSource)) {
-		return imageSource.default;
-	}
-
-	return imageSource;
-}
-
-export function isStaticRequire(x: string | StaticImport): x is StaticRequire {
-	if (typeof x === 'string') {
-		return false;
-	}
-
-	if ('default' in x && typeof x.default === 'object') {
-		return true;
-	}
-
-	return false;
-}
+export type LocalImageSource = StaticImport;
+export type RemoteImageSource = string;
