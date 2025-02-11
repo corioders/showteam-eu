8fa8fe0fbe0cc4f07a6b417259e11cb68858e522
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-02-11 22:47:58 +0100

    fix remote image not working in development

--- src/media/image/size.ts
+++ src/media/image/size.ts
@@ -2,27 +2,15 @@
 // Unauthorized copying of this file, via any medium is strictly prohibited
 // Proprietary and confidential
 // Written by Wiktor Jurkiewicz <watjurk@gmail.com> and Artur Mucowski <artur@mucowski.pl>, October 2024
-import type { ErrorReturnPromise } from '@/error';
+import { ErrorReturn, safe } from '@/error';
 import sizeOf from 'buffer-image-size';
 
 export interface ImageSize {
 	width: number;
 	height: number;
+	type: "bmp" |"cur" |"dds" |"gif" |"ico" |"jpg" |"png" |"psd" |"svg" |"webp"
 }
 
-export async function imageSize(src: string): ErrorReturnPromise<ImageSize> {
-	try {
-		const response = await fetch(src);
-		const responseArrayBuffer = await response.arrayBuffer();
-		const responseBuffer = Buffer.from(responseArrayBuffer);
-		const size = sizeOf(responseBuffer);
-		const imageSize = {
-			width: size.width,
-			height: size.height,
-		};
-
-		return [imageSize, null];
-	} catch (err) {
-		return [null, err];
-	}
+export  function imageSize(responseBuffer: Buffer): ErrorReturn<ImageSize> {
+	return safe(() => sizeOf(responseBuffer) as ImageSize) 
 }
