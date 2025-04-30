e68a7255e7bb1bc20c7c2f6d4f6de326d366e6b8
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-04-30 13:46:00 +0200

    refactor firebase auth

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -4,35 +4,51 @@
 // Written by Wiktor Jurkiewicz <watjurk@gmail.com> and Artur Mucowski <artur@mucowski.pl>, April 2025
 
 import type { ErrorReturnPromise } from '@/error/index.js';
-import type { FirebaseApp } from 'firebase/app';
-import { type UserCredential, createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
+import type { FirebaseApp, FirebaseError } from 'firebase/app';
+import { type UserCredential, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { firebaseSafePromise } from './error.js';
 
-const REQUIREMENTS_REGEX = /\[(?<requirements>.*)\]/;
 export async function firebaseSignupUserWithEmailAndPassword(firebaseApp: FirebaseApp, email: string, password: string): ErrorReturnPromise<UserCredential> {
 	const auth = getAuth(firebaseApp);
 
-	const [userCredentials, userError] = await firebaseSafePromise(() => createUserWithEmailAndPassword(auth, email, password));
-	if (userError) {
-		switch (userError.code) {
-			case 'auth/email-already-in-use':
-				return [null, new Error('Email already in use', { cause: userError })];
-			case 'auth/invalid-email':
-				return [null, new Error('Invalid email', { cause: userError })];
-			case 'auth/operation-not-allowed':
-				return [null, new Error('Operation not allowed', { cause: userError })];
-			case 'auth/password-does-not-meet-requirements': {
-				const requirements = userError.message.match(REQUIREMENTS_REGEX);
-				const requirementsText = requirements?.groups?.['requirements'] ?? '';
-
-				return [null, new Error(`Weak password: ${requirementsText}`, { cause: userError })];
-			}
-			default:
-				return [null, new Error(`Unknown error: ${userError.message}`, { cause: userError })];
-		}
+	const [createUserCredentials, createUserError] = await firebaseSafePromise(() => createUserWithEmailAndPassword(auth, email, password));
+	if (createUserError) {
+		return [null, switchFirebaseLoginError(createUserError)];
 	}
 
-	return [userCredentials, null];
+	return [createUserCredentials, null];
+}
+
+export async function firebaseSigninUserWithEmailAndPassword(firebaseApp: FirebaseApp, email: string, password: string): ErrorReturnPromise<UserCredential> {
+	const auth = getAuth(firebaseApp);
+
+	const [loginUserCredentials, loginUserError] = await firebaseSafePromise(() => signInWithEmailAndPassword(auth, email, password));
+	if (loginUserError) {
+		return [null, switchFirebaseLoginError(loginUserError)];
+	}
+
+	return [loginUserCredentials, null];
+}
+
+const REQUIREMENTS_REGEX = /\[(?<requirements>.*)\]/;
+function switchFirebaseLoginError(error: FirebaseError): Error {
+	switch (error.code) {
+		case 'auth/email-already-in-use':
+			return new Error('Email already in use', { cause: error });
+		case 'auth/invalid-email':
+			return new Error('Invalid email', { cause: error });
+		case 'auth/operation-not-allowed':
+			return new Error('Operation not allowed', { cause: error });
+		case 'auth/invalid-credential':
+			return new Error('Invalid credential', { cause: error });
+		case 'auth/password-does-not-meet-requirements': {
+			const requirements = error.message.match(REQUIREMENTS_REGEX);
+			const requirementsText = requirements?.groups?.['requirements'] ?? '';
+			return new Error(`Weak password: ${requirementsText}`, { cause: error });
+		}
+		default:
+			return new Error(`Unknown error: ${error.message}`, { cause: error });
+	}
 }
 
 type IsLoggedInSyncPromise = Promise<boolean>;
