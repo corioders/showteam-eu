32a33ffa577fa858b09b615c7f85ba1d55e00b7f
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-05-17 22:17:48 +0200

    add link component

---
+++ src/ui/link.tsx
@@ -0,0 +1,6 @@
+import NextLink from 'next/link';
+import type { ComponentProps } from 'react';
+
+export function Link({ prefetch, ...props }: ComponentProps<typeof NextLink>) {
+	return <NextLink prefetch={prefetch ?? true} {...props} />;
+}
