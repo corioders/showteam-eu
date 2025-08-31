19bccb3168f8b2375ff29fe8c9cebb2882d5c6e5
Wiktor Jurkiewicz <watjurk@gmail.com>
2025-08-30 20:55:04 -0400

    add outside Link support

--- src/type/index.ts
+++ src/type/index.ts
@@ -3,3 +3,7 @@ import type { ReactNode } from "react";
 export interface Children {
 	children: ReactNode;
 }
+
+export interface AsChild {
+	asChild?: boolean;
+}
--- src/ui/link.tsx
+++ src/ui/link.tsx
@@ -4,6 +4,11 @@ import type { ComponentProps } from "react";
 /**
  * https://h.corioders.com/cstd-next/link
  */
-export function Link({ prefetch, ...props }: ComponentProps<typeof NextLink>) {
-	return <NextLink prefetch={prefetch ?? true} {...props} />;
+export function Link({ prefetch, href, rel, target, ...props }: ComponentProps<typeof NextLink>) {
+	const isInternal = href.toString().startsWith("/");
+	if (isInternal) {
+		return <NextLink href={href} prefetch={prefetch ?? true} rel={rel} target={target} {...props} />;
+	}
+
+	return <a href={href.toString()} rel={rel ?? "noopener noreferrer"} target={target ?? "_blank"} {...props} />;
 }
