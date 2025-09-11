763a23ea4b80fb7c71b14c545630083a16604927
artalatarta <artur@mucowski.pl>
2025-09-12 00:14:23 +0100

    remove useless dependencies

--- src/development/index.tsx
+++ src/development/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { invalidateDriveCMS } from "cstd-ts/next/invalidate-drive-cms.js";
-import { Trash2Icon } from "lucide-react";
 import { type ReactPortal, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
@@ -104,10 +103,33 @@ export function CoriodersDevelopmentOverlay(): ReactPortal | null {
 			>
 				<span className="dev-tools-indicator-label">Invalidate Cache</span>
 				<span className="dev-tools-indicator-value">
-					<Trash2Icon />
+					<TrashIcon />
 				</span>
 			</button>
 		</>,
 		menuContainer,
+	);
+}
+
+function TrashIcon() {
+	return (
+		<svg
+			fill="none"
+			height="24"
+			stroke="currentColor"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth="2"
+			viewBox="0 0 24 24"
+			width="24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<title>Trash</title>
+			<path d="M10 11v6" />
+			<path d="M14 11v6" />
+			<path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+			<path d="M3 6h18" />
+			<path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+		</svg>
 	);
 }
--- src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-// TODO: Change the name of this file. We don't do "util"
-
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
-}
